3392c39154cbf2a1a9c920ca424dd59af4e1cc4a
toddpham12 <minhtaipham32@gmail.com>
2023-05-16 22:28:30 +0700

    (...)Update appsetting.json

---
+++ API/Migrations/20230516150857_UpdatedDb.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecom_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "avatar",
+                table: "users",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "avatar",
+                table: "users",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000);
+        }
+    }
+}
--- DTO/DTO/Entities/User.cs
+++ DTO/DTO/Entities/User.cs
@@ -6,7 +6,6 @@ namespace Ecom_API.DTO.Entities
     [Table("users")]
     public class User : BaseEntity
     {
-        [Required]
         [StringLength(1000)]
         public string username { get; set; }
         [Required]
@@ -19,14 +18,11 @@ namespace Ecom_API.DTO.Entities
         [StringLength(200)]
         public string email { get; set; }
         [StringLength(1000)]
-        public string? avatar { get; set; }
-        [Required]
+        public string avatar { get; set; }
         [StringLength(20)]
         public string phone { get; set; }
-        [Required]
         [StringLength(2000)]
         public string address { get; set; }
-        [Required]
         public bool isAdmin { get; set; }
         public List<int>? order_ids { get; set; }
     }
